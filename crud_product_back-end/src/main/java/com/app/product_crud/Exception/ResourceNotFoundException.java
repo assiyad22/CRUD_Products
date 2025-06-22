9a063b8bd14199f8097de3cfe731fb790156ca05
assiyad22 <118220265+assiyad22@users.noreply.github.com>
2025-06-22 21:56:53 +0100

    Adding ResourceNotFoundException class

--- crud_product_back-end/src/main/java/com/app/product_crud/Exception/ResourceNotFoundException.java
+++ crud_product_back-end/src/main/java/com/app/product_crud/Exception/ResourceNotFoundException.java
@@ -3,6 +3,6 @@ package com.app.product_crud.Exception;
 public class ResourceNotFoundException extends RuntimeException{
 
     public ResourceNotFoundException(String message) {
-        super(message);
+      super(message);
     }
 }
