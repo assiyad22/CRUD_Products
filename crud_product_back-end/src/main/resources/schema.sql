751885788ed297678673ca01255b7c2274aa2a7d
assiyad22 <118220265+assiyad22@users.noreply.github.com>
2025-06-22 22:21:10 +0100

    Creating products table schema

--- crud_product_back-end/src/main/resources/schema.sql
+++ crud_product_back-end/src/main/resources/schema.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS products (
+    
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     price DOUBLE PRECISION CHECK (price > 0)
+    
 );
 
 
